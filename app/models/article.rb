2ebc23fb041dac3d0e7364f398ad3ae13b5e1280
Adrian Schultze <adrianschultze@gmail.com>
2020-06-20 18:09:35 +0200

    added stuff

---
+++ app/models/article.rb
@@ -0,0 +1,20 @@
+class Article < ApplicationRecord
+  def create
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def show
+  end
+
+  private
+
+  def something; end
+end
